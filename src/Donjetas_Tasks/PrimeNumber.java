31ed57c5c03921d9719b1ef319c4124fb79b08e7
Ekaterine_Cydeo <vako_eko@yahoo.com>
2023-06-07 19:41:43 -0400

    My Prime Task

---
+++ src/Donjetas_Tasks/PrimeNumber.java
@@ -0,0 +1,27 @@
+package Donjetas_Tasks;
+
+public class PrimeNumber {
+
+    public static boolean isPrime(int number){
+
+        if (number < 2){
+            return false;
+        }
+
+        for (int i = 2; i <= Math.sqrt(number) ; i++) {
+            if (number % i == 0){
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+
+        System.out.println(isPrime(17));
+        System.out.println(isPrime(14));
+
+    }
+}
