523ca8c4802e3da05a165ad1d16ae1a88965a814
Ekaterine_Cydeo <vako_eko@yahoo.com>
2023-06-07 20:31:02 -0400

    created Pi task

---
+++ src/MakePi1.java
@@ -0,0 +1,18 @@
+import java.util.Arrays;
+
+public class MakePi1 {
+
+    public class MakePi {
+
+        public  void main(String[] args) {
+            System.out.println(Arrays.toString(makePi()));
+        }
+        public  int[] makePi() {
+
+
+            int[] pi = {3, 1, 4};
+
+            return pi;
+        }
+    }
+}
